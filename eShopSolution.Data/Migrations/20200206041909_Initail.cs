a5f4336e1b0ae836de0ebd27ca2bd0656551f897
Michael Hien <hientvok2014@gmail.com>
2020-02-06 11:27:56 +0700

    design database

---
+++ eShopSolution.Data/Migrations/20200206041909_Initail.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class Initail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2020, 2, 6, 11, 19, 8, 620, DateTimeKind.Local).AddTicks(7959),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 1, 31, 14, 17, 23, 45, DateTimeKind.Local).AddTicks(9455));
+
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "7003a0e5-050f-450e-bb83-85085d30afa2");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "f4e72a03-8733-4b38-a72f-9680eb0cf2a8", "AQAAAAEAACcQAAAAECCsE6/2f5dA/iEHvLbHo1Yn8DaG41RFUDh0EGRzXdwXHd1s0lSvM+H2XY4MQ0qiaQ==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 2, 6, 11, 19, 8, 643, DateTimeKind.Local).AddTicks(6088));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 1, 31, 14, 17, 23, 45, DateTimeKind.Local).AddTicks(9455),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 2, 6, 11, 19, 8, 620, DateTimeKind.Local).AddTicks(7959));
+
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "fba117d7-06b5-4c61-abcd-43a1ef7e0aa2");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "1298a9e3-fdba-4dd9-83d3-7b123eddded2", "AQAAAAEAACcQAAAAEL/CRFH2Ilm6CVYJ6ygyWugYf/JQcpwALzBZxtAkMLvMuwCStioi7RKrHKTwlyHVuQ==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 1, 31, 14, 17, 23, 67, DateTimeKind.Local).AddTicks(3650));
+        }
+    }
+}
